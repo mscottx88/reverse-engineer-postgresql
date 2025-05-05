6b82243ba564e6a13fb360dda18258633e7cac38
Michael Scott <michael.scott123@t-mobile.com>
2025-05-05 16:11:01 -0700

    feat: WIP

--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,16 @@ import * as utils from './utils';
 
 export const pool: pg.Pool = new pg.Pool({ allowExitOnIdle: true, max: 100 });
 
+/**
+ * This function executes the queries located in ./src/queries.ts against the source schema
+ * declared in the environment variable INPUT_SCHEMA. Those queries in turn produce DDL statements
+ * that are executed against the target schema declared in the OUTPUT_SCHEMA environment variable.
+ *
+ * The outcome is the source schema is replicated in its object structure into the target schema,
+ * thereby generating a pristine database for an end-to-end test.
+ *
+ * @returns Promise<void>
+ */
 export async function cloneInputSchema(): Promise<void> {
   const queries = [
     Queries.listFunctions,
@@ -53,6 +63,28 @@ export async function cloneInputSchema(): Promise<void> {
   }
 }
 
+/**
+ * Drop the test schema.
+ *
+ * **WARNING**
+ * The schema identified by the OUTPUT_SCHEMA environment variable is literally dropped.
+ *
+ * @returns Promise<void>
+ */
+export async function dropTestSchema(): Promise<void> {
+  await pool.query(utils.sql`DROP SCHEMA :I${process.env.OUTPUT_SCHEMA} CASCADE`);
+}
+
+/**
+ * Initialize the source schema.
+ *
+ * **WARNING**
+ * The schema identified by the INPUT_SCHEMA environment variable is actually dropped and recreated.
+ *
+ * This function can be executed using the command `npm run init`.
+ *
+ * @returns Promise<void>
+ */
 export async function initSourceSchema(): Promise<void> {
   await pool.query(utils.sql`DROP SCHEMA IF EXISTS :I${process.env.INPUT_SCHEMA} CASCADE`);
   await pool.query(utils.sql`CREATE SCHEMA :I${process.env.INPUT_SCHEMA}`);
@@ -61,6 +93,13 @@ export async function initSourceSchema(): Promise<void> {
   await pool.query(statements);
 }
 
+/**
+ * Setup the source schema.
+ *
+ * This function can be executed using the command `npm run init`.
+ *
+ * @returns Promise<void>
+ */
 export async function setupTestSchema(): Promise<void> {
   await pool.query('CREATE SEQUENCE IF NOT EXISTS public.test_schema');
   const { rows } = await pool.query<{ nextval: string }>(
@@ -71,8 +110,4 @@ export async function setupTestSchema(): Promise<void> {
 
   await pool.query(utils.sql`CREATE SCHEMA :I${testSchema}`);
   process.env.OUTPUT_SCHEMA = testSchema;
-}
-
-export async function teardownTestSchema(): Promise<void> {
-  await pool.query(utils.sql`DROP SCHEMA :I${process.env.OUTPUT_SCHEMA} CASCADE`);
 }
